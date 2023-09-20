517c64da01d907ba012e3578a6ff07eabe2867ff
Dylan Higgins <higgindy@mail.uc.edu>
2023-09-20 16:24:02 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int itemPrice = 20;
+        double salesTax = 0.0;
+
+        salesTax = itemPrice * .05;
+        System.out.println("The price of the item is: " + itemPrice + " and the sales tax is " + salesTax);
+
+    }
+}
